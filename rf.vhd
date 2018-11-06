973f7de2c36e068c565783bdb63ae5fbec5b7c4a
Tenzing Rabgyal <rabgyaltenzing@gmail.com>
2018-11-06 12:09:30 -0500

    Added rf.vhd file

---
+++ rf.vhd
@@ -0,0 +1,32 @@
+----------------------------------------------------------------------------------
+-- Company: NYU Tandon AHD
+-- Engineer: 
+-- 
+-- Create Date: 11/06/2018
+-- Design Name: 
+-- Module Name: control - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: Register File for MIPS32-AHD 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity RF is
+--  Port ( );
+end RF;
+
+architecture Behavioral of RF is
+
+begin
+
+
+end Behavioral;
