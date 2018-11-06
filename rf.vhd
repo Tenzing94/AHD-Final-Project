a95989b3a8aae04259e2b908dfe7463ae5b16d9f
Tenzing Rabgyal <rabgyaltenzing@gmail.com>
2018-11-06 12:11:51 -0500

    Revision 0.02 - Corrected some typos.

--- rf.vhd
+++ rf.vhd
@@ -4,7 +4,7 @@
 -- 
 -- Create Date: 11/06/2018
 -- Design Name: 
--- Module Name: control - Behavioral
+-- Module Name: rf - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -14,17 +14,18 @@
 -- 
 -- Revision:
 -- Revision 0.01 - File Created
+-- Revision 0.02 - Corrected some typos.
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity RF is
+entity rf is
 --  Port ( );
-end RF;
+end rf;
 
-architecture Behavioral of RF is
+architecture Behavioral of rf is
 
 begin
 
