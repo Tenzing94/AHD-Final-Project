2e16e79e17147277fa9b8b6b51471e34b20e7094
brentru <brent@adafruit.com>
2018-11-26 19:10:21 -0500

    update testbench to incl dmem,2

--- TestBench and Screenshots/tb_top_module.vhd
+++ TestBench and Screenshots/tb_top_module.vhd
@@ -5,6 +5,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.all;
+use STD.TEXTIO.ALL;
 
 entity tb_top_module is
 end tb_top_module;
@@ -13,18 +14,20 @@ architecture testbench of tb_top_module is
 
 component top_module is
     Port ( clk : in STD_LOGIC;
-           rst : in STD_LOGIC;
-           output : out STD_LOGIC_VECTOR (31 downto 0);
-           bit_flags : out STD_LOGIC_VECTOR (8 downto 0);
-           hal : out STD_LOGIC
-           backdoor_input_button : in std_logic;
-           backdoor_input_values : in std_logic_vector(15 downto 0);
-          );
+       rst : in STD_LOGIC;
+       outputA : out STD_LOGIC_VECTOR (31 downto 0);
+       outputB : out STD_LOGIC_VECTOR (31 downto 0);
+       bit_flags : out STD_LOGIC_VECTOR (8 downto 0); -- LED output
+       hal : out STD_LOGIC;
+       backdoor_input_button : in STD_LOGIC;
+       backdoor_input_values : in STD_LOGIC_VECTOR (15 downto 0)
+      );
 end component;
 
     -- // Signals // --
     signal tRst : std_logic := '0';
-    signal tOut : std_logic_vector(31 downto 0); -- dout
+    signal tOutA : std_logic_vector(31 downto 0); -- doutA
+    signal tOutB : std_logic_vector(31 downto 0); -- doutB
     signal tBit_Flags : std_logic_vector(8 downto 0); -- led output
     signal tHal : std_logic; -- HALT signal
     -- dmem signals
@@ -37,19 +40,19 @@ end component;
     constant cycle_time : time := 50000000 ps; -- avg + buffer time to reach a HALT command
     
 begin
-    -- // Testbench Components // --   
      
     -- Top Module
     UUT: top_module port map(
             clk => tClk,
             rst => tRst,
-            output => tOut,
+            outputA => tOutA,
+            outputB => tOutB,
             bit_flags => tBit_Flags,
             hal => tHal,
             backdoor_input_button => tBackdoorInput,
             backdoor_input_values => tBackdoorInputVals);
-            
-            
+   
+
    -- Clock process (how the clock should behave)
     clk_process :process
     begin
@@ -65,7 +68,19 @@ begin
        -- reset the CPU     
        -- hold reset state high for 100ns
        tRst <= '1';
-       -- TODO: Add in dmem configuration
+       wait for 20 ns;
+       tRst <= '0';
+       tBackdoorInputVals <= "0000000000000000";
+       wait for clk_period/2;
+       tBackdoorInput <= '1';
+       wait for clk_period/2;
+
+       tBackdoorInputVals <= "0000000000000000";
+       wait for clk_period/2;
+       tBackdoorInput <= '1';
+       wait for clk_period/2;
+        
+              
        wait for clk_period;
 
        -- start cpu (starts the PC) 
