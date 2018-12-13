cef99d49fe1c60aac80004d5ae82d2510f6c0613
Tenzing Rabgyal <rabgyaltenzing@gmail.com>
2018-12-13 18:18:16 -0500

    I changed the slow internal clock from 10khz to 20khz. This is because the professor wants the internal clock to be atleast 20khz. We can make it faster if we want to.

--- FPGA_Implementation_Files/clk_slow.vhd
+++ FPGA_Implementation_Files/clk_slow.vhd
@@ -33,12 +33,14 @@ end clk_slow;
 architecture Behavioral of clk_slow is
     signal temporal: STD_LOGIC;
     -- signal counter : integer range 0 to 49999999 := 0; -- 100MHZ/1 = 100M. 50% duty cycle is 50M. Which is 0 to 49.99M
-    signal counter : integer range 0 to 4999 := 0; -- 10KHz
+    -- signal counter : integer range 0 to 4999 := 0; -- 10KHz
+    signal counter : integer range 0 to 2499 := 0; -- 20KHz
 begin
     frequency_divider: process (clk_in) begin
         if rising_edge(clk_in) then
             -- if (counter = 49999999) then -- 1Hz
-            if (counter = 4999) then -- 10KHz
+            -- if (counter = 4999) then -- 10KHz
+            if (counter = 2499) then -- 20KHz
                 temporal <= NOT(temporal); -- if clk was high, then switch it to low since it has completed the period
                 counter <= 0;
             else
